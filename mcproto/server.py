2b9d1fd7f236df89858e5a785649c08ff5048354
Gaming32 <soujournme@gmail.com>
2021-01-24 15:02:02 -0600

    Update server.py

--- mcproto/server.py
+++ mcproto/server.py
@@ -14,6 +14,22 @@ class Connection:
         return f'<{self.address}: {self.connection}>'
 
 
+class Handshake:
+    protocol_version: int
+    local_address: str
+    local_port: int
+    _next_state: int
+
+    def __init__(self, proto, addr, port, state) -> None:
+        self.protocol_version = proto
+        self.local_address = addr
+        self.local_port = port
+        self._next_state = state
+
+    def __repr__(self) -> str:
+        return f'<Handshake to "{self.local_address}:{self.local_port}" with protocol version {self.protocol_version} (next state is {(None, "Ping", "Login")[self._next_state]})>'
+
+
 class Server:
     _events: dict[str, list[Callable[..., None]]]
     _singular_event_defaults: dict[str, Callable[..., Any]]
@@ -31,7 +47,7 @@ class Server:
         self.connections = {}
         self.protocol_version = protocol_version
 
-    def _default_status(self, conn: Connection, *args):
+    def _default_status(self, conn: Connection, handshake: Handshake):
         return {}
 
     def _invoke_singular_event(self, evname, *args, **kwargs):
@@ -56,11 +72,10 @@ class Server:
         local_addr = read_string(stream)
         local_port = int.from_bytes(stream.read(2), 'big', signed=False)
         next_state = read_var_int(stream)
-        return (proto, local_addr, local_port, next_state)
+        return Handshake(proto, local_addr, local_port, next_state)
 
-    def _send_status(self, conn: Connection, handshake):
-        get_packet_safe(conn.connection)
-        result = self._invoke_singular_event('status', conn, *handshake[:-1])
+    def _send_status(self, conn: Connection, handshake: Handshake):
+        result = self._invoke_singular_event('status', conn, handshake)
         json_data = json.dumps(result)
         stream = io.BytesIO()
         write_string(stream, json_data)
@@ -75,6 +90,23 @@ class Server:
             stream.write(number.to_bytes(8, 'big', signed=True))
             conn.connection.send(make_packet(0x01, stream.getvalue()))
 
+    def _login(self, conn: Connection, handshake: Handshake):
+        # packid, pack = get_packet_safe(conn.connection)
+        # if packid != 0x00:
+        #     self._invoke_event('disallowed', conn, 'first login packet not 0x00')
+        #     conn.close()
+        # stream = io.BytesIO(pack)
+        # username = read_string(stream)
+        username = 'jojo'
+        # Skip encryption for now
+        uuid = 0
+        stream = io.BytesIO()
+        stream.write(uuid.to_bytes(16, 'big', signed=False))
+        write_string(stream, username)
+        pack = make_packet(0x02, stream.getvalue())
+        conn.connection.send(pack)
+        self._invoke_event('login', uuid, username, handshake)
+
     def on(self, event, *callbacks):
         if event in self._singular_events:
             if not callbacks:
@@ -83,6 +115,7 @@ class Server:
                 raise ValueError(f'only one callback is supported for the event {event!r}')
             else:
                 self._singular_events[event] = callbacks[0]
+            return
         self._events.setdefault(event, [])
         if not callbacks:
             self._events[event].clear()
@@ -99,7 +132,10 @@ class Server:
             handshake_result = self._handshake(conn)
             if handshake_result is None:
                 self._invoke_event('disallowed', new_connection, 'first packet not 0x00')
+                conn.close()
                 continue
-            self._invoke_event('handshake', conn, *handshake_result)
-            if handshake_result[-1] == 1:
+            self._invoke_event('handshake', new_connection, handshake_result)
+            if handshake_result._next_state == 1:
                 self._send_status(new_connection, handshake_result)
+            else:
+                self._login(new_connection, handshake_result)
